64535e853e0bbf080cd808d59ce6788dbce88eff
M Haidar Hanif <git@mhaidarhanif.com>
2025-04-19 20:20:21 +0700

    fix demo page

--- demo.js
+++
@@ -1,8 +0,0 @@
-alert("Hello World of JavaScript");
-console.log("Hallo World");
-console.log("Hallo World");
-console.log("Hallo World of javaScript");
-console.log("Hallo World of JavaScript");
-console.log("Hallo World of JavaScript");
-console.log();
-c;
