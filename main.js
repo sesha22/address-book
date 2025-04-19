d76741c242aa5b184eb80dc286431e63436f45fe
Galuh Wasesa <surat.sesha@gmail.com>
2025-04-19 18:32:12 +0700

    add country name

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const contacts = [
     age: 36,
     email: "example.com",
     phone: "+62-1234-56789",
+    address: "Blitar",
+    country: "Indonesia",
   },
   {
     id: 2,
@@ -12,6 +14,8 @@ const contacts = [
     age: 31,
     email: "example.com",
     phone: "+62-1234-56789",
+    address: "Madiun",
+    country: "Indonesia",
   },
   {
     id: 3,
@@ -19,23 +23,20 @@ const contacts = [
     age: 89,
     email: "example.com",
     phone: "+62-1234-56789",
+    address: "Pare-pare",
+    country: "Jerman",
   },
 ];
 
 for (let index = 0; index < contacts.length; index++) {
   const contact = contacts[index];
-  console.log(contact.name, contact.email);
 
-  console.log(`Name: ${contact.name}
-   Email : ${contact.email}
-   Phone : ${contact.phone}
-   `);
-}
-
-console.log(`
-   Name : $ {contact.name}
-   Age : $contact.age} year old
+  console.log(`
+   Name : ${contact.name}
+   Age : ${contact.age} year old
    Email : ${contact.email}
    address : ${contact.address}
    Phone : ${contact.phone}
-   Country : ${contact.country}`);
+   Country : ${contact.country}
+   `);
+}
